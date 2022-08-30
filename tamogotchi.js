bbfb5e50d9c93c5a7bd8b1f02bc320c629226b68
SoNomad <nurmuhammad16203@gmail.com>
2022-08-30 14:21:45 +0300

    изменены некоторые параметры

--- tamogotchi.js
+++ tamogotchi.js
@@ -1,21 +1,21 @@
 const tamogotchi = {
-    name: 'egg',
+    name: 'Димон',
     meal: 1,
     energy: 2,
     mood: 4,
     getStatus: function() {
-      let meal = `Я не голоден (${this.meal})`
-      let energy = `Я не хочу спать (${this.energy})`
-      let mood = `Мне не скучно (${this.mood})`;
+      let meal = `${this.name} не голоден (${this.meal})`
+      let energy = `${this.name} не хочучет спать (${this.energy})`
+      let mood = `${this.name} не скучает (${this.mood})`;
 
       if (this.meal < 3) {
-        meal = `Я голоден (${this.meal})`
+        meal = `${this.name} голоден (${this.meal})`
       }   
       if (this.energy < 3) {
-        energy = `Я хочу спать (${this.energy})`
+        energy = `${this.name} хочет спать (${this.energy})`
       }
       if (this.mood < 3) {
-        mood = `Мне скучно (${this.mood})`
+        mood = `${this.name} умирает от скуки (${this.mood})`
       }
       if (this.meal < 1 || this.energy < 1 || this.mood < 1) {
         console.log(this.name + ' умер.')
@@ -25,13 +25,29 @@ const tamogotchi = {
       console.log(`Имя: ${this.name}, Еда: ${meal}, Энергия: ${energy}, Настроение: ${mood}.`)},
     
     setName: function(name) {this.name = name},
-    toEat: function(){if (this.meal < 5) {this.meal++; this.mood--}},
-    toSleep: function(){if (this.energy < 5) {this.energy++; this.meal--}},
-    toPlay: function(){if (this.mood < 5) {this.mood++; this.energy--}},
+    toEat: function() {
+      if (this.meal < 5) {
+        this.meal++
+      }this.mood--
+    },
+    
+    toSleep: function(){
+      if (this.energy < 5) {
+        this.energy++; 
+      }this.meal--
+    },
+    
+    toPlay: function(){
+      if (this.mood < 5) {
+        this.mood++
+      }this.energy--
+    },
     
     hesoyam: function(){
-      this.energy = 4
-      this.meal = 4
-      this.mood = 4
+      this.energy = 5
+      this.meal = 5
+      this.mood = 5
+      console.log(this.name + ' познал счастье (все параметры равны 5)')
     }
   }
+
